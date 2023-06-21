0135717998f5aa996b78badc6cdcc256899c1c18
molhamalrashed <molham.alrd@gmail.com>
2023-06-21 12:39:31 +0200

    some changes

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -21,6 +21,12 @@ export const initQuestionPage = () => {
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
+
+      let checkbox = document.createElement('input');
+      checkbox.type = 'radio';
+      checkbox.name = 'answer';
+      answerElement.insertBefore(checkbox, answerElement.firstChild);
+
     answersListElement.appendChild(answerElement);
   }
 
